7767e7cb73fca65f488ce51ae005c00b83f1d8ea
Gladys-nimo <wkanyora10@gmail.com>
2021-03-29 14:40:43 +0300

    added butter knife and array adapters

--- app/src/test/java/com/moringaschool/myrestaurant/MainActivityTest.java
+++ app/src/test/java/com/moringaschool/myrestaurant/MainActivityTest.java
@@ -1,21 +1,23 @@
 package com.moringaschool.myrestaurant;
 
-
-
 import android.content.Intent;
 import android.os.Build;
 import android.widget.TextView;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowActivity;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.robolectric.Shadows.shadowOf;
+
 
 @RunWith(RobolectricTestRunner.class)
-@Config(sdk = {Build.VERSION_CODES.O_MR1})
 public class MainActivityTest {
     private MainActivity activity;
 
@@ -26,10 +28,13 @@ public class MainActivityTest {
                 .resume()
                 .get();
     }
-
     @Test
-    public void validateTextViewContent() {
-        TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
-        assertTrue("My Restaurants".equals(appNameTextView.getText().toString()));
+    public void clickingLogin_shouldStartLoginActivity() {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        activity.findViewById(R.id.findRestaurantsButton).performClick();
+
+        Intent expectedIntent = new Intent(activity, RestaurantsActivity.class);
+        Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
     }
 }
